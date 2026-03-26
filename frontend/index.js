9022cbec20c9efd34efd371b99f901ef11aece35
Zuriel Aviles <avilez@rpi.edu>
2026-03-25 21:57:01 -0400

    Added Flask app outline will build on this.

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import App from './app'; // Update this path to reflect your structure
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
